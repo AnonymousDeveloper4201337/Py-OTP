34af4a5f69c04110aaad5bae19befca7800a92f3
EnumCoder <captaindoes@gmail.com>
2015-10-20 19:14:34 -0400

    Initial code commit

---
+++ src/connection/ConnectionManager.py
@@ -0,0 +1,54 @@
+from direct.directnotify import DirectNotifyGlobal
+from direct.task.TaskManagerGlobal import *
+from direct.task.Task import Task
+from pandac.PandaModules import *
+from panda3d.core import *
+
+class ConnectionManager(QueuedConnectionManager):
+	notify = DirectNotifyGlobal.directNotify.newCategory("ConnectionManager")
+
+	def __init__(self, port_address, backlog=10000):
+		self.cManager = QueuedConnectionManager()
+		self.cListener = QueuedConnectionListener(self.cManager, 0)
+		self.cReader = QueuedConnectionReader(self.cManager, 0)
+		self.cWriter = ConnectionWriter(self.cManager, 0)
+		self.activeConnections = []
+		self.port_address = port_address
+		self.backlog = backlog
+
+		if self.port_address:
+			self.setup()
+
+	def setup(self):
+		self.socket = self.cManager.openTCPServerRendezvous(self.port_address, self.backlog)
+		if self.socket:
+			self.cListener.addConnection(self.socket)
+			taskMgr.add(self.socketListener, 'Poll the connection listener')
+			taskMgr.add(self.socketReader, 'Poll the connection reader')
+
+	def socketListener(self, task):
+		if self.cListener.newConnectionAvailable():
+			rendezvous = PointerToConnection()
+			netAddress = NetAddress()
+			newConnection = PointerToConnection()
+
+			if self.cListener.getNewConnection(rendezvous, netAddress, newConnection):
+				newConnection = newConnection.p()
+				self.activeConnections.append(newConnection)
+				self.cReader.addConnection(newConnection)
+
+		return Task.cont
+
+	def socketReader(self, task):
+		if self.cReader.dataAvailable():
+			datagram = NetDatagram()
+
+			if self.cReader.getData(datagram)
+				self.handleDatagram(datagram)
+
+		return Task.cont
+
+	def handleDatagram(self, datagram):
+		connection = datagram.getConnection()
+		dgi = PyDatagramIterator(datagram)
+		messageType = dgi.getUint16()
---
+++ src/messagedirector/MessageDirector.py
@@ -0,0 +1,86 @@
+from src.connection.ConnectionManager import ConnectionManager
+from src.connection.protocol import *
+
+class MDParticipantInterface:
+
+	channelAllocator = None
+	canClearChannel = False
+
+	def __init__(self, base_class):
+		self.base_class = base_class
+
+	def handleDatagram(self):
+		if messageType == CONTROL_SET_CHANNEL:
+			self.registerChannel(dgi.getUint64(), connection)
+		elif messageType == CONTROL_REMOVE_CHANNEL:
+			self.unregisterChannel(dgi.getUint64(), connection)
+		elif messageType == CONTROL_MESSAGE:
+			self.base_class.routeMessageToChannel(dgi.getUint64(), dgi.getDatagram(), connection)
+		elif messageType == CONTROL_ADD_RANGE:
+			self.addChannelRange(dgi.getUint64())
+		elif messageType == CONTROL_REMOVE_RANGE:
+			self.removeChannelRange()
+		elif messageType == CONTROL_ADD_POST_REMOVE:
+			self.addPostRemove(dgi.getUint64())
+		elif messageType == CONTROL_CLEAR_POST_REMOVE:
+			if canClearChannel:
+				self.clearPostRemove(dgi.getUint64())
+			else:
+				return # participant was not authorized to remove channel.
+		else:
+			return # Could not handle the incoming datagram!
+
+	def registerChannel(self, channel, connection):
+		if channel not in self.base_class.channels:
+			if channel is None:
+				return # No channel was specified.
+			else:
+				self.base_class.channels[channel] = connection
+		else:
+			return # Channel: is already assigned!
+
+	def unregisterChannel(self, channel, connection):
+		if channel in self.base_class.channels:
+			if self.base_class.channels[channel] == connection:
+				del self.base_class[channel]
+			else:
+				return # Channel: was assigned to the wrong connection!
+		else:
+			return # Channel: was never registered!
+
+	def addChannelRange(self, channelRange):
+		pass
+
+	def removeChannelRange(self):
+		pass
+
+	def addPostRemove(self, channel):
+		pass
+
+	def clearPostRemove(self, channel):
+		pass
+
+class MessageDirector(ConnectionManager, MDParticipantInterface):
+
+	channels = {}
+	interestZones = set()
+
+	def __init__(self, port_address=7100):
+		ConnectionManager.__init__(port_address=port_address)
+		MDParticipantInterface.__init__(self, base_class=self)
+
+	def handleDatagram(self, datagram):
+		ConnectionManager.handleDatagram(self, datagram)
+		MDParticipantInterface.handleDatagram(self, datagram)
+
+	def routeMessageToChannel(self, channel, datagram, connection):
+		if channel in self.channels:
+			if self.channels[channel] != connection:
+				self.cWriter.send(datagram, self.channel[channel])
+			else:
+				return # Channel: Tried to send a datagram to its self.
+		else:
+			return # Channel: Tried to send a datagram to un-assigned channel!
+
+	def addInterest(self, parentId, zoneId): # TODO!
+		self.interestZones.add(zoneId)
