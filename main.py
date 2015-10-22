7e2d0c97409af4185be9211c7911594cf86bc6a5
EnumCoder <captaindoes@gmail.com>
2015-10-21 20:12:25 -0400

    Added in all the required message types.

--- main.py
+++ main.py
@@ -13,6 +13,22 @@ __builtin__.base.md = MessageDirector()
 from src.clientagent.ClientAgent import ClientAgent
 __builtin__.base.ca = ClientAgent()
 
+# StateServer:
+from src.stateserver.StateServer import StateServer
+__builtin__.base.ss = StateServer()
+
+# EventLogger:
+from src.eventlogger.EventLogger import EventLogger
+__builtin__.base.el = EventLogger()
+
+# DatabaseStateServer:
+from src.databasestateserver.DatabaseStateServer import DatabaseStateServer
+__builtin__.base.dbss = DatabaseStateServer()
+
+# DatabaseServer
+from src.databaseserver.DatabaseServer import DatabaseServer
+__builtin__.base.db = DatabaseServer()
+
 # Tests:
 #from tests.test_clientagent import test_clientagent
 
---
+++ src/stateserver/StateServer.py
@@ -0,0 +1,39 @@
+from src.connection.ConnectionManager import ConnectionManager
+from direct.directnotify import DirectNotifyGlobal
+from src.connection.protocol import *
+from direct.distributed.PyDatagram import PyDatagram
+from direct.distributed.PyDatagramIterator import PyDatagramIterator
+
+class StateServer(ConnectionManager):
+    notify = DirectNotifyGlobal.directNotify.newCategory("StateServer")
+    notify.setInfo(True)
+
+    ourChannel = 100002
+    activeConnections = []
+
+    # Connections
+    connection = None
+    socket = None
+    
+    def __init__(self, host_address=7100, ip_address='localhost'):
+        ConnectionManager.__init__(self, host_address=host_address, ip_address=ip_address)
+    
+    def serverStarted(self, port):
+        self.notify.info("Opened connection on port %d" % port)
+    
+    def registerChannel(self, channel):
+        dg = PyDatagram()
+        dg.addUint16(CONTROL_SET_CHANNEL)
+        dg.addUint64(channel)
+        self.cWriter.send(dg, self.connection)
+    
+    def unregisterChannel(self, channel):
+        dg = PyDatagram()
+        dg.addUint16(CONTROL_REMOVE_CHANNEL)
+        dg.addUint64(channel)
+        self.cWriter.send(dg, self.connection)
+    
+    def handleDatagram(self, datagram):
+        connection = datagram.getConnection()
+        dgi = PyDatagramIterator(datagram)
+        message_type = dgi.getUint16()
